f83f0f87b7cc4e5c15155ebb9e6ec8e0de049d0f
Douglas  Wiliam <dougl.wil@gmail.com>
2017-12-19 11:14:32 -0300

    Add files via Upload, the first commit

---
+++ GUI.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+
+base = Tk()
+
+imgX = PhotoImage(file='./Assets/X.png')
+imgO = PhotoImage(file='./Assets/O.png')
+imgEmpty = PhotoImage(file='./Assets/empty.png')
+
+
+class botaoJogo(Button):
+    def acao(self):
+        if self.imagem == imgEmpty:
+            self.imagem = imgX
+            self.b.config(image=self.imagem, width="60",height="60")
+    
+        elif self.imagem == imgX:
+            self.imagem = imgO
+            self.b.config(image=self.imagem, width="60",height="60")
+        
+        else:
+            self.imagem = imgEmpty
+            self.b.config(image=self.imagem, width="60",height="60")
+            
+    def __init__(self,master):
+        self.imagem = imgEmpty
+        self.master = master
+        self.b = Button(self.master, command=self.acao)
+        self.b.config(image=self.imagem, width="60",height="60")
+        
+    
+
+TR = botaoJogo(base) 
+TC = botaoJogo(base) 
+TL = botaoJogo(base)
+MR = botaoJogo(base)
+MC = botaoJogo(base)
+ML = botaoJogo(base)
+BR = botaoJogo(base)
+BC = botaoJogo(base)
+BL = botaoJogo(base)
+
+TR.b.grid(row=0, column=0)
+TC.b.grid(row=0, column=1)
+TL.b.grid(row=0, column=2)
+MR.b.grid(row=1, column=0)
+MC.b.grid(row=1, column=1)
+ML.b.grid(row=1, column=2)
+BR.b.grid(row=2, column=0)
+BC.b.grid(row=2, column=1)
+BL.b.grid(row=2, column=2)
+
+base.mainloop()
+
