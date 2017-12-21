282f9654a4b00fc6aba97e2834466084d20b3d01
Douglas Wiliam <dougl.wil@gmail.com>
2017-12-20 22:00:43 -0300

    Change of identation

--- client.py
+++ client.py
@@ -1,22 +1,26 @@
+#Cliente
 import socket
 
 
 class conexao:
-    def __init__(self):
-        self.HOST = '127.0.0.1'
-        self.PORT = 5001
-        self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.dest = (self.HOST, self.PORT)
-    def conectar(self):
-        try:
-            self.tcp.connect(self.dest)
-        except ConnectionRefusedError:
-            return False
-        return True
+	def __init__(self):
+		self.HOST = '127.0.0.1'
+		self.PORT = 5001
+		self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.dest = (self.HOST, self.PORT)
+	
+	def conectar(self):
+		try:
+			self.tcp.connect(self.dest)
+		except ConnectionRefusedError:
+			return False
+		return True
 
-    def mandaMsg(self,msg):
-        self.tcp.send(bytes(msg, 'utf-8'))
-
+	def mandaMsg(self, msg):
+		self.tcp.send(bytes(msg, 'utf-8'))
+	
+	def recebeMsg(self):
+		msg =  self.tcp.recv(1024)
 
 
 
